7c7d8ce10e0d4b0febeb8fa1a05ef2d54bbbcc02
lillebitteditte <ditt2047@stud.kea.dk>
2022-10-13 10:59:23 +0200

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("Hello World");
+        System.out.println("How is your day so far?"); //flere spørgsmaal
+
+        int hour = 10;
+        int minute = 59;
+        System.out.print("The current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.print(minute);
+        System.out.println(".");
+    }
+}
